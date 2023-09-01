722e5e2dd56a437b9f9f71c569dc5c5474455b86
KeerthiDG786 <keerthidgyadav@gmail.com>
2023-09-01 10:47:26 +0530

    commiting

---
+++ Hibernate/src/main/java/com/mit/Employee.java
@@ -0,0 +1,33 @@
+package com.mit;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+	@Id
+	private int eid;
+	private String ename;
+	private int eage;
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public int getEage() {
+		return eage;
+	}
+	public void setEage(int eage) {
+		this.eage = eage;
+	}
+	
+	
+
+}
---
+++ Hibernate/src/main/java/com/mit/HibernateUtil.java
@@ -0,0 +1,36 @@
+package com.mit;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;  
+  
+/** 
+ * 
+ * @author Raichand 
+ */  
+  
+public class HibernateUtil {      
+private static SessionFactory sessionFactory;  
+private static ServiceRegistry serviceRegistry;  
+private static SessionFactory  createSessionFactory() {  
+        try {  
+            // Create the SessionFactory from hibernate.cfg.xml  
+            Configuration configuration = new Configuration();  
+            configuration.configure("hibernate.cfg.xml");  
+            System.out.println("Hibernate Annotation Configuration loaded");               
+//            serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();  
+            System.out.println("Hibernate Annotation serviceRegistry created");               
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);               
+            return sessionFactory;  
+        }  
+        catch (Throwable ex) {  
+            // Make sure you log the exception, as it might be swallowed  
+            System.err.println("Initial SessionFactory creation failed." + ex);  
+            throw new ExceptionInInitializerError(ex);  
+        }    
+}  
+       public static SessionFactory getSessionFactory() {  
+        if(sessionFactory == null) sessionFactory = createSessionFactory();  
+        return sessionFactory;  
+    }  
+  } 
